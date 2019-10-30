9c8de5a7254af963fbb5eb71c6373d181f619873
sunqi <1366033452@qq.com>
2019-10-30 19:37:52 +0800

    Test

--- js-cloud-demo-server2/src/main/java/com/sq/jsclouddemoserver/JsCloudDemoServerApplication.java
+++ js-cloud-demo-server2/src/main/java/com/sq/jsclouddemoserver/JsCloudDemoServerApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 //本地新开发的功能
 //远程修改
 //修改
-//2019/10/30
+//2019/10/30 22
 @SpringBootApplication
 @EnableEurekaServer
 public class JsCloudDemoServerApplication {
