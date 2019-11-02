a673c19c09d9755bcf6bc9d9b21916be38451ac8
sunqi <1366033452@qq.com>
2019-11-02 11:28:15 +0800

    Merge branch 'sq'
    # Conflicts:
    #	js-cloud-demo-server2/src/main/java/com/sq/jsclouddemoserver/JsCloudDemoServerApplication.java

--- js-cloud-demo-server2/src/main/java/com/sq/jsclouddemoserver/JsCloudDemoServerApplication.java
+++ js-cloud-demo-server2/src/main/java/com/sq/jsclouddemoserver/JsCloudDemoServerApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 //本地新开发的功能
 //远程修改
 //修改
-//sun
+//sunSunSUN
 @SpringBootApplication
 @EnableEurekaServer
 public class JsCloudDemoServerApplication {
